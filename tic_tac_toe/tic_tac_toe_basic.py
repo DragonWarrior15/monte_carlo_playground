7e52eba818d13b821b3823ba8fb7408166f1f649
Vaibhav Ojha <vaibhav.ojha60@gmail.com>
2017-07-20 16:20:38 +0530

    updated code to get observations of variation of wins/ties as more and more games are played, across mutiple simulations

--- tic_tac_toe/tic_tac_toe_basic.py
+++ tic_tac_toe/tic_tac_toe_basic.py
@@ -3,7 +3,10 @@ a simple python two machine player script where
 the machines play using monte carlo simlulation and update
 the scores in each cell using the end result of the game.
 '''
+
 import random
+import matplotlib.pyplot as plt
+import os
 
 def get_empty_positions(curr_board):
     n = len(curr_board)
@@ -89,40 +92,75 @@ def pretty_print_board(board):
         print ([board[i][j] for j in range(n)])
     print ('########################')
 
-num_simulations = 1
-num_matches = 100
-board_size = 3
-win_list = []
-# the main simulation
-for _ in range(num_simulations):
-    # initialize the score tracker
-    score_board = []
-    for i in range(board_size):
-        score_board.append([0] * board_size)
-
-    for _ in range(num_matches):
-        # initialize the board
-        game_board = []
+def main():
+    # the main simulation
+    for sim_no in range(num_simulations):
+        # initialize the score tracker
+        score_board = []
         for i in range(board_size):
-            game_board.append([0] * board_size)
+            score_board.append([0] * board_size)
+        win_list = []
 
-        # play the game, X (-1) pays first
-        player_1 = 1 if random.randint(0, 1) else -1
-        player_2 = -1 * player_1
+        for _ in range(num_matches):
+            # initialize the board
+            game_board = []
+            for i in range(board_size):
+                game_board.append([0] * board_size)
 
-        curr_chance = -1
-        while (check_if_win(game_board) == board_size):
-            # pretty_print_board(game_board)
-            next_move = select_next_move(score_board, game_board)
-            game_board[next_move[0]][next_move[1]] = curr_chance
-            curr_chance *= -1
-        winner = check_if_win(game_board)
-        win_list.append(winner)
-        score_board = update_score_board(score_board, game_board, winner)
-    # check how the score board looks
-    pretty_print_board(score_board)
-    # pretty_print_board(game_board)
+            # play the game, X (-1) pays first
+            player_1 = 1 if random.randint(0, 1) else -1
+            player_2 = -1 * player_1
 
-with open('winner_list.csv', 'w') as f:
-    # print (win_list)
-    f.write('\n'.join([str(x) for x in win_list]))
+            curr_chance = -1
+            while (check_if_win(game_board) == board_size):
+                # pretty_print_board(game_board)
+                next_move = select_next_move(score_board, game_board)
+                game_board[next_move[0]][next_move[1]] = curr_chance
+                curr_chance *= -1
+            winner = check_if_win(game_board)
+
+            # update the winner list
+            win_list.append(winner)
+
+            # update the score board
+            score_board = update_score_board(score_board, game_board, winner)
+        # check how the score board looks
+        print (sim_no)
+        pretty_print_board(score_board)
+        # pretty_print_board(game_board)
+
+    # with open('winner_list.csv', 'w') as f:
+        # print (win_list)
+        # f.write('\n'.join([str(x) for x in win_list]))
+
+
+    # plot the cumulative no of wins/tie
+        winner_list_cumul = [[sum([1 if win_list[i] == -1 else 0 for i in range(j)]) for j in range(num_matches)], 
+                             [sum([1 if win_list[i] == 0  else 0 for i in range(j)]) for j in range(num_matches)], 
+                             [sum([1 if win_list[i] == 1  else 0 for i in range(j)]) for j in range(num_matches)]]
+
+
+        dir_name = "tic tac toe with board size " + str(board_size) + " and " + str(num_matches) + " matches"
+        try:
+            os.mkdir(dir_name)
+        except OSError:
+            pass
+        fig_name = os.path.join(dir_name, str(sim_no) + ".png")
+        plt.figure()
+        plt.plot([i for i in range(1, num_matches + 1)], winner_list_cumul[0], label = "X")
+        plt.plot([i for i in range(1, num_matches + 1)], winner_list_cumul[1], label = "Tie")
+        plt.plot([i for i in range(1, num_matches + 1)], winner_list_cumul[2], label = "O")
+        # plt.xticks([i for i in range(1, num_matches)])
+        plt.xlabel('Match No')
+        plt.ylabel('Total Cumulative Count')
+        plt.legend(loc = 2)
+        plt.title(dir_name + " sim no " + str(sim_no))
+        plt.savefig(fig_name, dpi = 300)
+
+
+
+num_simulations = 10
+num_matches = 1000
+board_size = 2
+if (__name__ == "__main__"):
+    main()
